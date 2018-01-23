2d2ca309fda99143c4266a90c61a8266abf3e454
unknown <kozlovpavel2ch@gmail.com>
2018-01-23 05:10:52 -0500

    asd

--- Complex/Complex/Properties/AssemblyInfo.cs
+++ Complex/Complex/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Complex")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Complex")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("896b7dea-39ce-429b-8d89-8f513977a8fa")]
+[assembly: Guid("e61c9e26-ef54-402a-bff9-5dc3997d1d3a")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ maxmin/maxmin/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = int.Parse(Console.ReadLine());
+            StreamReader file1 = new StreamReader(@"C:\test\file1.txt");
+            string s = file1.ReadLine();
+            string[] s1 = s.Split(' ');
+            int n;
+            int max = -1000000;
+            int min = 1000000;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                n = int.Parse(s1[i]);
+                if (max < n)
+                    max = n;
+                if (min > n)
+                    min = n;
+            }
+            Console.WriteLine("Max" + "= " + max + "\n" + "Min" + "= " + min);
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+    
+}
