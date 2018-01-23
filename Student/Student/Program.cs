318c6b94e03b5f6771927665e6ccdb85c53af0f5
unknown <kozlovpavel2ch@gmail.com>
2018-01-23 05:29:44 -0500

    asd

---
+++ Student/Student/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    class Student
+    {
+        public string firstName;
+        public string lastName;
+        public float gpa;
+
+        public Student()
+        {
+            firstName = "Pavel";
+            lastName = "Kozlov";
+            gpa = 4;
+        }
+
+        public override string ToString()
+        {
+            return this.firstName + " " + lastName + " " + gpa;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s = new Student();
+
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+
+        }
+    }
+}
